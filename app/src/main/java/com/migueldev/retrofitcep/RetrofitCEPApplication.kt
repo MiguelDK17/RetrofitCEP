33da3ca65675b80b2d5a37a37e50041e78597a2e
MiguelDK17 <miguelmf8633431@gmail.com>
2024-10-22 13:02:41 -0300

    Bugs relacionados ao Hilt corrigidos

--- app/src/main/java/com/migueldev/retrofitcep/RetrofitCEPApplication.kt
+++ app/src/main/java/com/migueldev/retrofitcep/RetrofitCEPApplication.kt
@@ -2,8 +2,9 @@ package com.migueldev.retrofitcep
 
 import android.app.Application
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
 
-@AndroidEntryPoint
+@HiltAndroidApp
 class RetrofitCEPApplication : Application(){
     override fun onCreate() {
         super.onCreate()
